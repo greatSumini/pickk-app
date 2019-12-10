10926265e298cb7b162f1b8e4a10630cdb2d4fa1
Choi Sumin <greatSumini@gmail.com>
2019-12-11 02:25:49 +0900

    [Chore] complete config setting

---
+++ src/config/index.tsx
@@ -0,0 +1,14 @@
+import env from 'react-native-config';
+
+const config = {
+  api: {
+    host: env.API_HOST,
+    timeout: 20000,
+  },
+};
+
+const API_HOST = config.api.host;
+
+export {API_HOST};
+
+export default config;
