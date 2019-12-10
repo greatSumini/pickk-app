5632f5eb7c7e70fa8447831e48a2e685019b5393
Choi Sumin <greatSumini@gmail.com>
2019-12-11 03:45:40 +0900

    [Chore] complete react-navigation setting

--- index.tsx
+++ index.tsx
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './src/App';
+import App from './src/app';
 import {name as appName} from './app.config.js';
 
 AppRegistry.registerComponent(appName, () => App);
