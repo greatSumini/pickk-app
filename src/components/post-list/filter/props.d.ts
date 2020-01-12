214dc24f200e008108bfca9d02c079ca1f421c91
Yang <ybh1760@gmail.com>
2020-01-13 03:04:20 +0900

    [Fix] fix conflict

--- src/components/post-list/filter/props.d.ts
+++ src/components/post-list/filter/props.d.ts
@@ -1,18 +1,10 @@
 type ButtonProps<T> = {
   value: T;
-<<<<<<< HEAD
-  setValue: (v) => void;
-};
-
-type FilterProps = {
-  postType: string;
-=======
   setValue: React.Dispatch<React.SetStateAction<T>>;
 };
 
 type FilterProps = {
   postType: '리뷰' | 'LOOK';
->>>>>>> b624cb18a9d40f1eb3cdac3ded77254a46ce9dd9
 };
 
 export default FilterProps;
