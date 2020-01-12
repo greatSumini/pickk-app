be04e209eaa749767a4c6ba726c9f45cf4d7532e
Yang <ybh1760@gmail.com>
2020-01-13 02:59:02 +0900

    [Enhance] add addSizeToImagePath to util/image-size-parser
    - delete addSizeToFilename in util/url-parser

--- src/lib/utils/url-parser.ts
+++ src/lib/utils/url-parser.ts
@@ -1,29 +1,14 @@
+import {addSizeToImagePath, ImageSize} from './image-size-parser';
+
 export const imageUriHandler = (
   titleType: string,
   titleImageUrl: string,
   titleYoutubeUrl: string,
+  size: ImageSize,
 ) => {
   if (titleType === 'IMAGE') {
-    return titleImageUrl;
+    return addSizeToImagePath(titleImageUrl, size);
   } else {
     return `https://img.youtube.com/vi/${titleYoutubeUrl}/sddefault.jpg`;
   }
 };
-
-export const addSizeToFilename = (filename, size) => {
-  if (!filename) {
-    return filename;
-  }
-  const _fileLen = filename.length;
-  const _lastDot = filename.lastIndexOf('.');
-
-  const _fileName = filename.substring(0, _lastDot);
-  const _fileExt = filename.substring(_lastDot, _fileLen);
-
-  return `${_fileName}_${size}${_fileExt}`;
-};
-
-export const xsmall = 'xsmall';
-export const small = 'small';
-export const medium = 'medium';
-export const large = 'large';
