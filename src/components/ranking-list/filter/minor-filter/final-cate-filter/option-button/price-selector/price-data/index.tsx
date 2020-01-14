84abb8e0f7cdfd29685c527c7cc1b48fde9d9e80
Yang <ybh1760@gmail.com>
2020-01-14 23:34:27 +0900

    [Feature] add price-bar & price-data to price-selector

--- src/components/ranking-list/filter/minor-filter/final-cate-filter/option-button/price-selector/price-data/index.tsx
+++ src/components/ranking-list/filter/minor-filter/final-cate-filter/option-button/price-selector/price-data/index.tsx
@@ -2,13 +2,38 @@ import React from 'react';
 import styled from 'styled-components/native';
 
 import {useRankFilterDrawerContext} from '@src/context/filter';
-import rem from '@src/constants/rem';
+import Text from '@src/modules/atoms/text';
+import {addCommaToNumber} from '@src/lib/utils/price-parser';
+import {MIN_PRICE} from '@src/components/ranking-list';
 
 export default function PriceData() {
   const rankFilterDrawerContext = useRankFilterDrawerContext();
   const {minPrice, maxPrice} = rankFilterDrawerContext.state;
 
-  return <Wrapper />;
+  const minPriceHandler = () => {
+    const result = addCommaToNumber((minPrice as any)._value);
+    if (result === null) {
+      return addCommaToNumber(MIN_PRICE);
+    }
+    return result;
+  };
+
+  const maxPriceHandler = () => {
+    return addCommaToNumber((maxPrice as any)._value);
+  };
+
+  return (
+    <Wrapper>
+      <Text level={3} fontWeight="bold">
+        {minPriceHandler() + ' ~ '}
+      </Text>
+      <Text level={3} fontWeight="bold">
+        {maxPriceHandler()}
+      </Text>
+    </Wrapper>
+  );
 }
 
-const Wrapper = styled.View({});
+const Wrapper = styled.View({
+  flexDirection: 'row',
+});
