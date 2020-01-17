a20cc6b5c1d26d85bc4442410209e2265a3a2771
Yang <ybh1760@gmail.com>
2020-01-17 16:57:13 +0900

    [Chore] Add ArrowLeft Icon to asset/icons/arrow

---
+++ src/assets/icons/arrow/left.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Svg, {Path} from 'react-native-svg';
+
+export default ({style, fill}) => (
+  <Svg style={style} viewBox="0 0 24 24">
+    <Path
+      fill={fill}
+      d="M9.3 5.3c.4-.4 1-.4 1.4 0 .4.4.4 1 0 1.4L6.4 11H20c.5 0 .9.4 1 .9v.1c0 .6-.4 1-1 1H6.4l4.3 4.3c.4.4.4.9.1 1.3l-.1.1c-.4.4-1 .4-1.4 0l-6-6-.1-.1-.1-.1v-.1-.1-.1-.1-.1-.1-.1-.1-.1-.1-.1-.1-.1s0-.1.1-.1l.1-.1-.1.1.1-.1 6-5.7z"
+    />
+  </Svg>
+);
