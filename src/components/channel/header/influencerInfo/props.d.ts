3f8a846f6946973af01758f21a63f1cbc4fae2c3
Yang <ybh1760@gmail.com>
2020-01-17 19:38:58 +0900

    [Implement] add InfleunceInfo

---
+++ src/components/channel/header/influencerInfo/props.d.ts
@@ -0,0 +1,12 @@
+type InfluenceInfoProps = {
+  data: {
+    name: string;
+    profileImageUrl: string;
+    channel_pickCount: number;
+    channel_titleImageUrl: string;
+    channel_totalViewCount: number;
+  };
+  edit: boolean;
+};
+
+export default InfluenceInfoProps;
