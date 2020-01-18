54fb218bb6a99b63da6ade226b5405cbfb8e40a6
Choi Sumin <greatSumini@gmail.com>
2020-01-19 02:59:27 +0900

    [Feature] add RecommendLookMoreButton

--- src/components/home/exhibition/recommend-look/more-button/index.tsx
+++ src/components/home/exhibition/recommend-look/more-button/index.tsx
@@ -1,9 +1,50 @@
 import React from 'react';
-
-import MoreButtonProps from './props';
+import styled from 'styled-components/native';
 
 import Text from '@src/modules/atoms/text';
+import TouchableCmp from '@src/modules/atoms/touchable-component';
+import Space from '@src/modules/atoms/space';
+import RefreshIcon from '@src/assets/icons/refresh';
+import rem from '@src/constants/rem';
+import colors from '@src/constants/colors';
 
-export default function RecommendLookMoreButton(props: MoreButtonProps) {
-  return <Text>나는 더 보기 버튼입니다.</Text>;
+import {PAGE_NUM} from '..';
+
+export type MoreButtonProps = {
+  current: number;
+  onPress: () => void;
+};
+
+export default function RecommendLookMoreButton({
+  current,
+  onPress,
+}: MoreButtonProps) {
+  return (
+    <Touchable onPress={onPress}>
+      <Wrapper>
+        <RefreshIcon
+          style={{width: rem(14), height: rem(14)}}
+          fill={colors.primary}
+        />
+        <Space direction='ROW' size={8} />
+        <Text level={1} fontWeight='medium'>
+          {`다른 LOOK 더 보기 (${current + 1}/${PAGE_NUM})`}
+        </Text>
+      </Wrapper>
+    </Touchable>
+  );
 }
+
+const Touchable = styled(TouchableCmp)({});
+
+const Wrapper = styled.View({
+  width: rem(228),
+  paddingVertical: rem(10),
+  borderRadius: rem(17),
+  flexDirection: 'row',
+  justifyContent: 'center',
+  alignItems: 'center',
+  borderStyle: 'solid',
+  borderWidth: rem(1),
+  borderColor: colors.regularGrey,
+});
