fb4108acae7d6c52938b97c91a279b1a2b66e0b5
Choi Sumin <greatSumini@gmail.com>
2020-01-19 01:36:03 +0900

    [Enhance] add ChevronRightIcon to HomeExhibitionHeader

--- src/components/home/exhibition/header/index.tsx
+++ src/components/home/exhibition/header/index.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 
 import Text from '@src/modules/atoms/text';
 import colors from '@src/constants/colors';
+import styled from 'styled-components/native';
+import ChevronRightIcon from '@src/assets/icons/chevron/right';
+import rem from '@src/constants/rem';
 
 export type ExhibitionHeaderProps = {
   title: string;
   description: string;
-  route: string;
+  route?: string;
 };
 
 export default function ExhibitionHeader({
@@ -15,13 +18,33 @@ export default function ExhibitionHeader({
   route,
 }: ExhibitionHeaderProps) {
   return (
-    <>
-      <Text level={3} fontWeight='bold'>
-        {title}
-      </Text>
-      <Text level={1} color={colors.secondary}>
-        {description}
-      </Text>
-    </>
+    <Wrapper>
+      <TextWrapper>
+        <Text level={3} fontWeight='bold'>
+          {title}
+        </Text>
+        <Text level={1} color={colors.secondary}>
+          {description}
+        </Text>
+      </TextWrapper>
+      {route && (
+        <ChevronRightIcon
+          style={{width: rem(20), height: rem(20)}}
+          fill={colors.primary}
+        />
+      )}
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.View({
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+});
+
+const TextWrapper = styled.View({
+  display: 'flex',
+  flexDirection: 'column',
+});
