ed35a0d56b4607a27b6d3f8161c90214d08f3e6d
Choi Sumin <greatSumini@gmail.com>
2020-01-19 02:59:57 +0900

    [Feature] add HomeExhibition/RecommendLook

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -6,6 +6,9 @@ import HomeHeader from './header';
 import HomeCarousel from './carousel';
 import HomeExhibition from './exhibition';
 import QuickMenu from './exhibition/quick-menu';
+import RecommendLook from './exhibition/recommend-look';
+import {ScrollView} from 'react-native';
+import Space from '@src/modules/atoms/space';
 
 export type HomeScreenProps = {
   navigation: NavigationDrawerProp & NavigationTabProp;
@@ -13,15 +16,19 @@ export type HomeScreenProps = {
 
 export default function HomeScreen(props: HomeScreenProps) {
   return (
-    <>
+    <ScrollView>
       <HomeHeader toggleDrawer={props.navigation.toggleDrawer} />
       <HomeCarousel />
-      <HomeExhibition
-        title='핔 200프로 이용하기'
-        description='핵심만 모았다!'
-        route={null}>
+      <HomeExhibition title='핔 200프로 이용하기' description='핵심만 모았다!'>
         <QuickMenu />
       </HomeExhibition>
-    </>
+      <HomeExhibition
+        title='오늘의 추천 LOOK'
+        description='코디 꿀팁 대방출!'
+        route={'sumin'}>
+        <RecommendLook />
+      </HomeExhibition>
+      <Space level={2} />
+    </ScrollView>
   );
 }
