227fc31d05f835c78577af3e92024905deb18458
Choi Sumin <greatSumini@gmail.com>
2020-01-18 22:28:05 +0900

    [Feature] add HomeHeader

--- src/components/home/header/index.tsx
+++ src/components/home/header/index.tsx
@@ -1,7 +1,44 @@
 import React from 'react';
+import styled from 'styled-components/native';
 
-import Text from '@src/modules/atoms/text';
+import IconButton from '@src/modules/atoms/buttons/icons';
+import HamburgetIcon from '@src/assets/icons/hamburger';
+import SearchIcon from '@src/assets/icons/search';
+import colors from '@src/constants/colors';
 
-export default function HomeHeader() {
-  return <Text>저는 홈 헤더</Text>;
+export type HomeHeaderProps = {
+  toggleDrawer: () => void;
+};
+
+export default function HomeHeader({toggleDrawer}: HomeHeaderProps) {
+  return (
+    <Wrapper>
+      <IconButton
+        onPress={toggleDrawer}
+        Icon={HamburgetIcon}
+        size={24}
+        fill={colors.white}
+      />
+      <IconButton
+        onPress={toggleDrawer}
+        Icon={SearchIcon}
+        size={24}
+        fill={colors.white}
+      />
+    </Wrapper>
+  );
 }
+
+const Wrapper = styled.View({
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  backgroundColor: 'transparent',
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  zIndex: 2,
+  width: '100%',
+  paddingVertical: 16,
+  paddingHorizontal: 20,
+});
--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,19 +1,14 @@
 import React from 'react';
-import {Button} from 'react-native';
 
-import Text from '@src/modules/atoms/text';
 import HomeScreenProps from './props';
+import HomeHeader from './header';
+import HomeCarousel from './carousel';
 
 export default function HomeScreen(props: HomeScreenProps) {
   return (
     <>
-      <Button
-        title="Drawer"
-        onPress={() => {
-          props.navigation.toggleDrawer();
-        }}
-      />
-      <Text>이니셜 커밋</Text>
+      <HomeHeader toggleDrawer={props.navigation.toggleDrawer} />
+      <HomeCarousel />
     </>
   );
 }
