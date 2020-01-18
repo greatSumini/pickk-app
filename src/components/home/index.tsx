068ac8ac8f59cdf80033a53a7d81cd42ac8f9ac7
Choi Sumin <greatSumini@gmail.com>
2020-01-19 01:25:39 +0900

    [Enhance] add QuickMenu to HomeScreen

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
+import {NavigationDrawerProp} from 'react-navigation-drawer';
+import {NavigationTabProp} from 'react-navigation-tabs';
 
-import HomeScreenProps from './props';
 import HomeHeader from './header';
 import HomeCarousel from './carousel';
+import HomeExhibition from './exhibition';
+import QuickMenu from './exhibition/quick-menu';
+
+export type HomeScreenProps = {
+  navigation: NavigationDrawerProp & NavigationTabProp;
+};
 
 export default function HomeScreen(props: HomeScreenProps) {
   return (
     <>
       <HomeHeader toggleDrawer={props.navigation.toggleDrawer} />
       <HomeCarousel />
+      <HomeExhibition
+        title='핔 200프로 이용하기'
+        description='핵심만 모았다!'
+        route={null}>
+        <QuickMenu />
+      </HomeExhibition>
     </>
   );
 }
