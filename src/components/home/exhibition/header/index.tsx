6690121a8c2809db2ce01a4fb8ea1873c650ee02
Choi Sumin <greatSumini@gmail.com>
2020-01-19 01:23:32 +0900

    [Feature] add HomeExhibitionHeader

--- src/components/home/exhibition/header/index.tsx
+++ src/components/home/exhibition/header/index.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
 
-import ExhibitionHeaderProps from './props';
-
 import Text from '@src/modules/atoms/text';
+import colors from '@src/constants/colors';
 
-export default function ExhibitionHeader(props: ExhibitionHeaderProps) {
-  return <Text>나는 기획전 헤더</Text>;
+export type ExhibitionHeaderProps = {
+  title: string;
+  description: string;
+  route: string;
+};
+
+export default function ExhibitionHeader({
+  title,
+  description,
+  route,
+}: ExhibitionHeaderProps) {
+  return (
+    <>
+      <Text level={3} fontWeight='bold'>
+        {title}
+      </Text>
+      <Text level={1} color={colors.secondary}>
+        {description}
+      </Text>
+    </>
+  );
 }
