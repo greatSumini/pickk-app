ea4e83f5bb4ded430a09f48502b789c4412f607c
Choi Sumin <greatSumini@gmail.com>
2020-01-19 20:43:41 +0900

    [Enhance] fix import order

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {ScrollView} from 'react-native';
 import {NavigationDrawerProp} from 'react-navigation-drawer';
 import {NavigationTabProp} from 'react-navigation-tabs';
 
@@ -7,7 +8,6 @@ import HomeCarousel from './carousel';
 import HomeExhibition from './exhibition';
 import QuickMenu from './exhibition/quick-menu';
 import RecommendLook from './exhibition/recommend-look';
-import {ScrollView} from 'react-native';
 import Space from '@src/modules/atoms/space';
 
 export type HomeScreenProps = {
