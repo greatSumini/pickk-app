b2bfaf67abb3d4acbff60ae004a7c589568c6041
Yang <ybh1760@gmail.com>
2020-01-21 02:04:24 +0900

    [Enhance] Add FilterWrapper to modules/molecules/filter

--- src/components/channel/filter/index.tsx
+++ src/components/channel/filter/index.tsx
@@ -1,14 +1,8 @@
 import React from 'react';
-import styled from 'styled-components/native';
 
 import PostFilter from './post';
 import ItemFilter from './item';
-import rem from '@src/constants/rem';
-import colors from '@src/constants/colors';
-
-type FilterWrapperProps = {
-  children: React.ReactNode;
-};
+import FilterWrapper from '@src/modules/molecules/filter/filter-wrapper';
 
 export default function Filter({navType, id}) {
   return (
@@ -18,18 +12,3 @@ export default function Filter({navType, id}) {
     </FilterWrapper>
   );
 }
-
-function FilterWrapper({children}: FilterWrapperProps) {
-  return <Wrapper>{children}</Wrapper>;
-}
-
-const Wrapper = styled.View({
-  width: '100%',
-  height: rem(36),
-  borderBottomWidth: rem(1),
-  borderBottomColor: colors.lightGrey,
-  flexDirection: 'row',
-  alignItems: 'center',
-  paddingHorizontal: rem(12),
-  justifyContent: 'space-between',
-});
