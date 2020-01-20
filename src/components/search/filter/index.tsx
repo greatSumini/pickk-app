12cde30b3a024c886f0b08f8009222bbffe99258
Yang <ybh1760@gmail.com>
2020-01-20 18:24:31 +0900

    [Feature] structure search directory

---
+++ src/components/search/filter/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components/native';
+
+export default function Filter() {
+  return <Wrapper></Wrapper>;
+}
+
+const Wrapper = styled.View({
+  flex: 1,
+});
---
+++ src/components/search/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components/native';
+
+export default function Search() {
+  return <Wrapper></Wrapper>;
+}
+
+const Wrapper = styled.SafeAreaView({
+  flex: 1,
+});
