73ad3378dfba9b5d331a63efe9ca7c4cf43b6d36
Yang <ybh1760@gmail.com>
2020-01-28 15:32:28 +0900

    [Chore] modify chevronDown icon

--- src/assets/icons/chevron/down.tsx
+++ src/assets/icons/chevron/down.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Svg, {Path} from 'react-native-svg';
 
-export default ({style, fill}) => (
-  <Svg style={style} viewBox="0 0 24 24">
-    <Path
-      fill={fill}
-      d="M12 13.6L6.7 8.3c-.4-.4-1-.4-1.4 0-.4.4-.4 1 0 1.4l6 6c.4.4 1 .4 1.4 0l6-6c.4-.4.4-1 0-1.4-.4-.4-1-.4-1.4 0L12 13.6z"
-    />
-  </Svg>
-);
+export default function ChevronDown({style, fill}) {
+  return (
+    <Svg style={style} viewBox='0 0 24 24'>
+      <Path
+        fill={fill}
+        d='M12 13.6L6.7 8.3c-.4-.4-1-.4-1.4 0-.4.4-.4 1 0 1.4l6 6c.4.4 1 .4 1.4 0l6-6c.4-.4.4-1 0-1.4-.4-.4-1-.4-1.4 0L12 13.6z'
+      />
+    </Svg>
+  );
+}
