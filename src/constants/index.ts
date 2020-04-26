3596c7ac48058c8f17726745e9da36277e42b9fc
greatSumini <greatsumini@gmail.com>
2020-04-26 12:26:34 +0900

    [Enhance] add constants/index,atoms/index

---
+++ src/constants/index.ts
@@ -0,0 +1,4 @@
+export * from './colors';
+export * from './dimensions';
+export {default as colors} from './colors';
+export {default as rem} from './rem';
---
+++ src/modules/atoms/index.ts
@@ -0,0 +1,5 @@
+export {default as Image} from './Img';
+export {default as Line} from './line';
+export {default as Text} from './text';
+export {default as TouchableCmp} from './touchable-component';
+export {default as Space} from './space';
