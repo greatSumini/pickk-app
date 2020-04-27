84a0eb40d8d04725e32237914e08bb6771174baa
greatSumini <greatsumini@gmail.com>
2020-04-28 01:02:28 +0900

    [Chore] build

---
+++ index.js
@@ -0,0 +1,10 @@
+"use strict";
+exports.__esModule = true;
+/**
+ * @format
+ */
+require("react-native-gesture-handler");
+var react_native_1 = require("react-native");
+var app_config_js_1 = require("./app.config.js");
+var app_1 = require("./src/app");
+react_native_1.AppRegistry.registerComponent(app_config_js_1.name, function () { return app_1["default"]; });
