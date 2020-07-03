f040c1296b4a61bf18b1a73eca725fa362d7e920
greatSumini <greatsumini@gmail.com>
2020-07-03 22:14:56 +0900

    [Chore] add config.django-api-host

--- config/index.ts
+++ config/index.ts
@@ -2,6 +2,7 @@ const config = {
   'node-env': 'production',
   'api-host': 'https://pickkapiserver.online/graphql',
   'onesignal-app-key': 'bbad55b9-36f5-42c9-add5-f6c35457bb81',
+  'django-api-host': 'https://api.pickk.xyz/v1',
 };
 
 export default config;
