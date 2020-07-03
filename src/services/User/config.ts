7a18d25c1a3c621cf299bb901c194ef7868e57d3
greatSumini <greatsumini@gmail.com>
2020-07-03 22:37:56 +0900

    [Feature] add user service

---
+++ src/services/User/config.ts
@@ -0,0 +1,32 @@
+import { IncomingMessage } from 'http';
+
+import { baseConfig } from '../Api';
+import { IUserInputDTO } from '@src/interfaces';
+import { ListRequestParams } from '@src/types';
+
+export const meReadConfig = (req?: IncomingMessage) =>
+  baseConfig(true, req).get('/users/me/');
+
+export const mePartialUpdateConfig = (updated: Partial<IUserInputDTO>) =>
+  baseConfig(true).patch('/users/me/', updated);
+
+export const meFollowingFeedItemsConfig = (params: ListRequestParams) =>
+  baseConfig(true).get('/users/me/following_feed/items/', { params });
+
+export const meFollowingFeedLooksConfig = (params: ListRequestParams) =>
+  baseConfig(true).get('/users/me/following_feed/looks/', { params });
+
+export const meFollowingFeedReviewsConfig = (params: ListRequestParams) =>
+  baseConfig(true).get('/users/me/following_feed/reviews/', { params });
+
+export const readConfig = (id: number, req?: IncomingMessage) =>
+  baseConfig(true, req).get(`/users/${id}/`);
+
+export const followConfig = (id: number, req?: IncomingMessage) =>
+  baseConfig(true, req).post(`/users/${id}/follow/`);
+
+export const unfollowConfig = (id: number, req?: IncomingMessage) =>
+  baseConfig(true, req).post(`/users/${id}/unfollow/`);
+
+export const isFollowingConfig = (id: number, req?: IncomingMessage) =>
+  !!id ? baseConfig(true, req).get(`/users/${id}/is_following/`) : null;
