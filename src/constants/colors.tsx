71bace95181feaeff68e21a31d2abb64a896a8fe
박효진 <gywlsp@baghyojin-ui-MacBookPro.local>
2020-07-13 13:44:26 +0900

    [Enhance] Add colors used in pickk-cli

--- src/constants/colors.tsx
+++ src/constants/colors.tsx
@@ -1,9 +1,21 @@
 export const WHITE = '#FFF';
-export const LIGHT_GREY = '#E7E7E7';
+export const LIGHT_GREY = '#F0F0F0';
 export const REGULAR_GREY = '#d8d8d8';
 export const MIDDLE_GREY = '#999999';
 export const BLACK = '#333';
 export const BACKGROUND_BLACK = '#222';
+export const HIGHLIGHT_PRIMARY = '#6937A1';
+export const SALE_RED = '#d95050';
+export const BORDER_GREY = '#e1e1e1';
+export const DARK_GREY = '#a9a9a9';
+export const DELIVERY_BLUE = '#8fc9e3';
+export const MEDIUM_LIGHT_RED = '#ea6969';
+export const DARK_PINK = '#ba727a';
+export const DARK_BLUE = '#2f447c';
+export const COCOA = '#55281d';
+export const DARK_ORANGE = '#cb753c';
+export const REDDISH_GRAY = '#a88383';
+export const ROSE_BROWN = '#563c3c';
 
 export default {
   primary: '#333',
