a1712472b0f60d82eeefc8ea6d0207f76b7a4812
박효진 <gywlsp@MacBook-Pro.local>
2020-07-14 19:29:45 +0900

    [Enhance] Add row, col to atom index

--- src/modules/atoms/index.ts
+++ src/modules/atoms/index.ts
@@ -5,3 +5,5 @@ export {default as TouchableCmp} from './touchable-component';
 export {default as Touchable} from './touchable';
 export {default as Space} from './space';
 export {default as Button} from './button';
+export {default as Row} from './row';
+export {default as Col} from './col';
