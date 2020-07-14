1002b6d835e1f08cc5a41ad9f7d1d6bfff75fa2e
박효진 <gywlsp@MacBook-Pro.local>
2020-07-14 18:37:01 +0900

    [Enhance] Add OrderItem confirm config, service

--- src/services/OrderItem/config.ts
+++ src/services/OrderItem/config.ts
@@ -3,3 +3,6 @@ import {baseConfig} from '../Api';
 
 export const listConfig = (req?: IncomingMessage) =>
   baseConfig(true, req).get('/order_items/');
+
+export const confirmConfig = (id: number, req?: IncomingMessage) =>
+  baseConfig(true, req).post(`/order_items/${id}/confirm/`);
