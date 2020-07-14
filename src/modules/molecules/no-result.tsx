c6c70d39ab2b81a8f95b057c047d6a0a9b57097a
박효진 <gywlsp@MacBook-Pro.local>
2020-07-14 19:31:49 +0900

    [Feature] Add NoResult

---
+++ src/modules/molecules/no-result.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import styled from 'styled-components/native';
+import {useNavigation} from '@react-navigation/native';
+
+import {rem} from '@src/constants';
+import {Space, Text, Col, Button} from '@src/modules/atoms';
+
+export type NoResultProps = {
+  icon?: JSX.Element;
+  text: string;
+  button?: {text: string; screen: string};
+};
+
+export default function NoResult({icon, text, button}: NoResultProps) {
+  const navigation = useNavigation();
+  return (
+    <Wrapper>
+      {icon && (
+        <>
+          {icon}
+          <Space level={4} />
+        </>
+      )}
+      <Text level={2} textAlign='center' preWrap>
+        {text}
+      </Text>
+      {button && (
+        <>
+          <Space level={2} />
+          <Button
+            title={button.text}
+            onPress={() => navigation.navigate(button.screen)}
+          />
+        </>
+      )}
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled(Col)({
+  paddingVertical: rem(10),
+});
