cf35a9a4d3ce06f30da5aea9f8bee19e82b77836
박효진 <gywlsp@MacBook-Pro.local>
2020-07-17 10:48:55 +0900

    [Feature] Add useOrderCancel hook

--- src/hooks/Order.ts
+++ src/hooks/Order.ts
@@ -1,6 +1,49 @@
 import {readConfig} from '@src/services/Order/config';
 import useRequest from './api/useRequest';
 import {IOrder} from '@src/interfaces/Order/IOrder';
+import {OrderState} from '@src/types';
 
 export const useOrder = (id: number, generateConfig?: any, initialData?: any) =>
   useRequest<IOrder>(generateConfig(readConfig(id), true), {initialData});
+
+export const useOrderCancel = (
+  id: number,
+  generateConfig?: any,
+  initialData?: any,
+) => {
+  const {data, error} = useRequest<IOrder>(
+    generateConfig(readConfig(id), true),
+    {initialData},
+  );
+  if (!data) {
+    return {
+      data,
+      error,
+    };
+  }
+
+  const newOrder = data.order
+    .filter(
+      (brand) =>
+        brand.orderItems.filter(
+          (item) =>
+            item.status === OrderState.PaymentFinished ||
+            item.status === OrderState.Pending,
+        ).length !== 0,
+    )
+    .map((brand) => {
+      return {
+        ...brand,
+        orderItems: brand.orderItems.filter(
+          (item) =>
+            item.status === OrderState.PaymentFinished ||
+            item.status === OrderState.Pending,
+        ),
+      };
+    });
+
+  return {
+    data: {...data, order: newOrder},
+    error,
+  };
+};
