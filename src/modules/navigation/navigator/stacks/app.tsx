3d0d766284a1c60185e549dfdc294775cad0ba27
박효진 <gywlsp@MacBook-Pro.local>
2020-07-17 11:05:27 +0900

    [Enhance] Add OrderCancel, ClaimComplete screen to app stack

--- src/modules/navigation/navigator/stacks/app.tsx
+++ src/modules/navigation/navigator/stacks/app.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
+import {StackNavigationProp} from '@react-navigation/stack';
 
 import MainTab from './main';
 import PostViewScreen from '@src/components/post-view';
@@ -7,13 +8,15 @@ import ChannelScreen from '@src/components/channel';
 import SearchScreen from '@src/components/search/index';
 import LoginScreen from '@src/components/login';
 import OrderListScreen from '@src/components/order-list';
-
-import {StackNavigationProp} from '@react-navigation/stack';
+import OrderCancelScreen from '@src/components/order-cancel';
+import ClaimCompleteScreen from '@src/components/order-claim/complete';
 
 export type AppStackParams = {
   Search: undefined;
   PostView: {id: number};
   Channel: {id: number};
+  OrderCancel: {id: number};
+  ClaimComplete: {type: 'cancel' | 'exchange' | 'refund'};
 };
 
 export type AppStackNavigationProps = StackNavigationProp<AppStackParams>;
@@ -28,5 +31,7 @@ export default () => (
     <AppStack.Screen name='Search' component={SearchScreen} />
     <AppStack.Screen name='Login' component={LoginScreen} />
     <AppStack.Screen name='OrderList' component={OrderListScreen} />
+    <AppStack.Screen name='OrderCancel' component={OrderCancelScreen} />
+    <AppStack.Screen name='ClaimComplete' component={ClaimCompleteScreen} />
   </AppStack.Navigator>
 );
