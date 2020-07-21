1b60d5c133f354daf9916654108999aa19f1f858
박효진 <gywlsp@MacBook-Pro.local>
2020-07-21 19:56:38 +0900

    [Feature] Add useQuestion hook

--- src/hooks/question.ts
+++ src/hooks/question.ts
@@ -2,7 +2,7 @@ import useRequest from './api/useRequest';
 
 import {QuestionListRequestParams, IQuestion} from '@src/interfaces';
 import {ListResponse} from '@src/types';
-import {listConfig} from '@src/services/Question/config';
+import {listConfig, readConfig} from '@src/services/Question/config';
 
 export const useQuestionList = (
   params: QuestionListRequestParams,
@@ -15,3 +15,9 @@ export const useQuestionList = (
       initialData,
     },
   );
+
+export const useQuestion = (
+  id: number,
+  generateConfig?: any,
+  initialData?: any,
+) => useRequest<IQuestion>(generateConfig(readConfig(id), true), initialData);
