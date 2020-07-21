5c0a1be75db0bf126d6929d4f8597c6fdb3a98c5
박효진 <gywlsp@MacBook-Pro.local>
2020-07-21 14:42:23 +0900

    [Feature] Extract OrderCancelProductInfo from OrderCancelProductCard

---
+++ src/components/order-cancel/product/info.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import {OrderItem} from '@src/types';
+import OrderCancelProductDescription, {
+  OrderCancelProductDescriptionProps,
+} from './description';
+import {rem} from '@src/constants';
+import {Space, Image} from '@src/modules/atoms';
+
+export type OrderCancelProductInfoProps = Pick<OrderItem, 'imageUrl'> &
+  OrderCancelProductDescriptionProps;
+
+function OrderCancelProductInfo(props: OrderCancelProductInfoProps) {
+  return (
+    <>
+      <Image
+        src={props.imageUrl}
+        size={50}
+        imgWidth={rem(50)}
+        imgHeight={rem(60.6)}
+      />
+      <Space direction='ROW' size={8} />
+      <OrderCancelProductDescription
+        {...(props as OrderCancelProductDescriptionProps)}
+      />
+    </>
+  );
+}
+
+export default React.memo(OrderCancelProductInfo);
